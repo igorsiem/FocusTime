cfbe417520b62c2029eda7d3345544c322402f11
Igor Siemienowicz <igor@qprise.com>
2021-04-13 07:04:11 +0800

    #2 intermediate check in

--- focustime/models/segment.py
+++ focustime/models/segment.py
@@ -83,7 +83,7 @@ class Segment:
     def begin(self,
             start = None,
             nominal_focus_duration=None,
-            nominal_break_duration=None
+            nominal_break_duration=None):
         """Commence the focusing segment
 
         Args:
--- tests/test_session.py
+++ tests/test_session.py
@@ -6,23 +6,28 @@ sys.path.insert(0, '../focustime')
 from focustime.models.session import Session
 
 def test_instantiation():
-   """Test basic instantiation of the Session class
+    """Test basic instantiation of the Session class
 
-   1. A `Session` can be instantiated with a:
-   
-      a. Nominal number of Segments denoting how many Segments are *planned*
-         for the Session
+    1. A `Session` can be instantiated with a:
 
-      b. A nominal long break duration, denoting the length of time that is
-         *planned* for the longer break in between segments
+       a. Nominal number of Segments denoting how many Segments are *planned*
+          for the Session
 
-   2. When a `Session` is instantiated, it has a `segments` member, which is
-       a list with zero elements.
-   """
+       b. A nominal long break duration, denoting the length of time that is
+          *planned* for the longer break in between segments
 
-   s = Session(nominal_number_of_segments=4,
-      nominal_long_break_duration=timedelta(minutes=15))
-   assert s != None
-   assert len(s.segments) == 0
-   assert s.nominal_number_of_segments == 4
-   assert s.nominal_long_break_duration == timedelta(minutes=15)
+    2. When a `Session` is instantiated, it has a `segments` member, which is
+        a list with zero elements.
+    """
+
+    s = Session(nominal_number_of_segments=4,
+       nominal_long_break_duration=timedelta(minutes=15))
+    assert s != None
+    assert len(s.segments) == 0
+    assert s.nominal_number_of_segments == 4
+    assert s.nominal_long_break_duration == timedelta(minutes=15)
+
+def test_create_and_begin_next_segment():
+    """Verify a Session's ability to create and being segments
+    """
+    pass
